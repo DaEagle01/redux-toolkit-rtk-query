b3b1e2846d93fe3c55265e931a9916b131510801
Fahad Bin Karim <fbinkarim01@gmail.com>
2024-01-20 21:13:35 +0600

    managing global state of products and cart using redux toolkit

---
+++ src/redux/features/cart/cartSlice.ts
@@ -0,0 +1,47 @@
+import { IProduct } from '@/types/globalTypes';
+import { createSlice } from '@reduxjs/toolkit';
+import type { PayloadAction } from '@reduxjs/toolkit';
+
+interface ICart {
+  products: IProduct[];
+  total: number;
+}
+
+const initialState: ICart = {
+  products: [],
+  total: 0,
+};
+
+const cartSlice = createSlice({
+  name: 'cart',
+  initialState,
+  reducers: {
+    addToCart: (state, action: PayloadAction<IProduct>) => {
+      const existing = state.products.find((p) => p._id === action.payload._id);
+      if (existing) {
+        existing.quantity = existing.quantity! + 1;
+      } else {
+        state.products.push({ ...action.payload, quantity: 1 });
+      }
+      state.total += action.payload.price;
+    },
+    removeOneFromCart: (state, action: PayloadAction<IProduct>) => {
+      const existing = state.products.find((p) => p._id === action.payload._id);
+      if (existing && existing.quantity! > 1) {
+        existing.quantity = existing.quantity! - 1;
+        state.total -= action.payload.price;
+      }
+    },
+    removeFromCart: (state, action: PayloadAction<IProduct>) => {
+      state.products = state.products.filter(
+        (p) => p._id !== action.payload._id
+      );
+      state.total -= action.payload.price * action.payload.quantity!;
+    },
+  },
+});
+
+export const { addToCart, removeOneFromCart, removeFromCart } =
+  cartSlice.actions;
+
+export default cartSlice.reducer;
